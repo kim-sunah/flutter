92332bbaf272a6eb210a4db9ece8fc5b4419403b
sunah <1004_sunah@naver.com>
2023-09-09 20:28:48 +0900

    상태관리 패키지 provider준비하기

--- mymemo/mymemo/lib/main.dart
+++ mymemo/mymemo/lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mymemo/memo_service.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => MemoService()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
